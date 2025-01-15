57d6567295d4414487e01ef3e9056c8eb89946ca
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-15 16:27:11 +0100

    upload

--- Front-End/Unit 3/Week-2/u3-w2-d3/Esercizio/app-react/src/App.jsx
+++ Front-End/Unit 3/Week-2/u3-w2-d3/Esercizio/app-react/src/App.jsx
@@ -1,21 +1,20 @@
 import NetHeader from "./Components/NetHeader";
-
 import NetFooter from "./Components/NetFooter";
-
 import NetFilms from "./Components/NetFilms";
-
 import NetNavBar from "./Components/NetNavBar";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-
-function App() {
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+const App = function() {
   return (
-    <>
+    <BrowserRouter>
       <NetNavBar />
-      <NetHeader />
-      <NetFilms />
-      <NetFooter />
-    </>
+      <NetHeader/>
+      <Routes>
+        <Route path='/Movies' element={<NetFilms/>} />
+      </Routes>
+      <NetFooter/>
+      
+    </BrowserRouter>
   );
 }
 
