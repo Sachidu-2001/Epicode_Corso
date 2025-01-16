e1353d98e62c06a0f19fb97fd6d884f694ab75d9
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-16 16:49:23 +0100

    commit

---
+++ Front-End/Unit 3/Week-1/u3-w1-d2/app-react/src/Components/MyFooter.jsx
@@ -0,0 +1,12 @@
+
+function MyFooter() {
+    return(
+        <div className="bg-dark">
+            <span className="text-white">
+                Epicode - Copyright 2025
+            </span>
+        </div>
+    )
+}
+
+export default MyFooter
