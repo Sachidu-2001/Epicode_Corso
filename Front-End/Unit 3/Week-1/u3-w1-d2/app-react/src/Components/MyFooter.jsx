b09cc0bc30be03cb44541468d51967816b327cac
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-16 17:36:26 +0100

    commit1

--- Front-End/Unit 3/Week-1/u3-w1-d2/app-react/src/Components/MyFooter.jsx
+++ Front-End/Unit 3/Week-1/u3-w1-d2/app-react/src/Components/MyFooter.jsx
@@ -3,8 +3,9 @@ function MyFooter() {
     return(
         <div className="bg-dark">
             <span className="text-white">
-                Epicode - Copyright 2025
+                Epicode - <span>Copyright 2025</span>
             </span>
+            <p className="text-white">Tutti i diritti riservati</p>
         </div>
     )
 }
