6e9f3afba8f73931c13f752e410506a49029bfba
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-17 01:32:11 +0100

    adding books

--- Front-End/Unit 3/Week-1/u3-w1-d2/app-react/src/App.jsx
+++ Front-End/Unit 3/Week-1/u3-w1-d2/app-react/src/App.jsx
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./Components/MyNav";
 import MyFooter from "./Components/MyFooter";
 import Welcome from "./Components/Welcome";
+import AllTheBooks from "./Components/AllTheBooks";
 
 function App() {
   return (
     <>
       <MyNav/>
       <Welcome/>
+      <AllTheBooks/>
       <MyFooter/>
     </>
   );
