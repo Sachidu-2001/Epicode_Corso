b520894fede453f9e70609cf88d2bd0d55a12f28
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-24 10:02:20 +0100

    commit

--- Front-End/Unit 3/Week-3/u3.w3.d4/react-type-app/src/components/NavBar.tsx
+++ Front-End/Unit 3/Week-3/u3.w3.d4/react-type-app/src/components/NavBar.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const NavbarComponent = () => {
   return (
-    <Navbar expand='md' className='bg-body-tertiary'>
+    <Navbar expand='md' className='bg-warning'>
       <Container className='d-flex justify-content-between'>
         <Link to='/' className='fw-bold text-black text-decoration-none fs-4'>
           SpaceFlight
         </Link>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
+          <Nav className='m-auto'>
             <Link to='/' className='text-black text-decoration-none'>
               Home
             </Link>
-            <Link to='/articles' className='text-black text-decoration-none'>
+            <Link to='/articles' className='text-black text-decoration-none ms-lg-3'>
               All Articles
             </Link>
           </Nav>
