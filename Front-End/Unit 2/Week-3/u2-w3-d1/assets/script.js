f122b6e22b9509ed96a85f9ae363420a175c6c5d
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-01 15:49:45 +0100

    commit

--- Front-End/Unit 2/Week-3/u2-w3-d1/assets/script.js
+++ Front-End/Unit 2/Week-3/u2-w3-d1/assets/script.js
@@ -4,10 +4,11 @@ const Person = function() {
     this.lastName = ''
     this.age = ''
     this.city = ''
+    this.address = ''
 
 
     this.presentation = () => {
-        return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city}`
+        return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city} in ${this.address}`
     }
 }
 
@@ -15,40 +16,46 @@ const person1 = new Person
 
 person1.firstName = 'Sachidu'
 person1.lastName = 'Fernando'
-person1.age = '23'
-person1.city = 'Milano'
+person1.age = '16'
+person1.city = 'Cornaredo'
+person1.address = 'Via Magenta 3'
 
 console.log(person1.presentation())
 
 
 const person2 = new Person
 
-person2.firstName = 'Dustin'
-person2.lastName = 'Farinaro'
-person2.age = '25'
-person2.city = 'San Zenone'
+person2.firstName = 'Sion'
+person2.lastName = 'Fernando'
+person2.age = '17'
+person2.city = 'Baggio'
+person2.address = 'Via Valle Atrona 5'
 
 console.log(person2.presentation())
 
 
-const Comparison = function (_firstName, _lastName,_age,_city) {
+const Comparison = function (_firstName, _lastName,_age,_city, _address) {
     this.firstName = _firstName
     this.lastName = _lastName
     this.age = _age
     this.city = _city
+    this.address = _address
 
     this.bigger = () => {
         if(person1.age >= person2.age) {
-            return `${person1.firstName}${person1.lastName} è più grande di ${person2.firstName} ${person2._lastName}`
+            return `${person1.firstName} ${person1.lastName} è più grande di ${person2.firstName} ${person2.lastName}`
         } else {
-            return `${person2.firstName}${person2.lastName} è più grande di ${person1.firstName} ${person1._lastName}`
+            return `${person2.firstName} ${person2.lastName} è più grande di ${person1.firstName} ${person1.lastName}`
         }
 
     }
+
+    this.girl = ()  => {
+        return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city} in ${this.address}`
+    }
 }
 
-const person1Comparison = new Comparison
-const person2Comparison = new Comparison
+const girl1 = new Comparison('Valentina','Cornelio','15','Lacchiarella','Via Puccini 4')
 
+console.log(girl1.girl())
 
-console.log(person1Comparison,person2Comparison)
