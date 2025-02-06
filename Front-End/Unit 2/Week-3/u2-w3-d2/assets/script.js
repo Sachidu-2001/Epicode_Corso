694e4f3869d1426d90be6f6f4e7b8625d344f022
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-06 14:41:27 +0100

    commit

--- Front-End/Unit 2/Week-3/u2-w3-d2/assets/script.js
+++ Front-End/Unit 2/Week-3/u2-w3-d2/assets/script.js
@@ -1,1 +1,19 @@
-const 
+const words = document.getElementById('paragraph')
+const btnSave = document.getElementById('save')
+const btnRemove = document.getElementById('remove')
+const input = document.getElementById('input')
+
+
+btnSave.addEventListener('click', function(e) {
+    e.preventDefault()
+    saveText()
+
+})
+
+function saveText() {
+    let inputName = localStorage.setItem('name', `${input.value}`)
+}
+
+function printText() {
+    
+}
