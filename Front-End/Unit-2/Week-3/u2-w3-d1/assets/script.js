daccd0ba7b7af6cda7d414a3b4881a06ded0d187
Sachidu-2001 <fsachi0001@gmail.com>
2025-02-17 21:41:41 +0100

    commit

--- Front-End/Unit-2/Week-3/u2-w3-d1/assets/script.js
+++ Front-End/Unit-2/Week-3/u2-w3-d1/assets/script.js
@@ -1,145 +1,140 @@
+const Person = function () {
+  this.firstName = "";
+  this.lastName = "";
+  this.age = "";
+  this.city = "";
+  this.address = "";
 
-const Person = function() {
-    this.firstName = ''
-    this.lastName = ''
-    this.age = ''
-    this.city = ''
-    this.address = ''
+  this.presentation = () => {
+    return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city} in ${this.address}`;
+  };
+};
 
+const person1 = new Person();
 
-    this.presentation = () => {
-        return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city} in ${this.address}`
+person1.firstName = "Sachidu";
+person1.lastName = "Fernando";
+person1.age = "16";
+person1.city = "Cornaredo";
+person1.address = "Via Magenta 3";
+
+console.log(person1.presentation());
+
+const person2 = new Person();
+
+person2.firstName = "Sion";
+person2.lastName = "Fernando";
+person2.age = "17";
+person2.city = "Baggio";
+person2.address = "Via Valle Atrona 5";
+
+console.log(person2.presentation());
+
+const Comparison = function (_firstName, _lastName, _age, _city, _address) {
+  this.firstName = _firstName;
+  this.lastName = _lastName;
+  this.age = _age;
+  this.city = _city;
+  this.address = _address;
+
+  this.bigger = () => {
+    if (person1.age >= person2.age) {
+      return `${person1.firstName} ${person1.lastName} è più grande di ${person2.firstName} ${person2.lastName}`;
+    } else {
+      return `${person2.firstName} ${person2.lastName} è più grande di ${person1.firstName} ${person1.lastName}`;
     }
-}
+  };
 
-const person1 = new Person
+  this.girl = () => {
+    return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city} in ${this.address}`;
+  };
+};
 
-person1.firstName = 'Sachidu'
-person1.lastName = 'Fernando'
-person1.age = '16'
-person1.city = 'Cornaredo'
-person1.address = 'Via Magenta 3'
+const result = new Comparison();
 
-console.log(person1.presentation())
+console.log(result.bigger()); // ricorda di richiamare una costante per ottenere il risulato//
 
+const girl1 = new Comparison(
+  "Valentina",
+  "Cornelio",
+  "15",
+  "Lacchiarella",
+  "Via Puccini 4"
+);
 
-const person2 = new Person
-
-person2.firstName = 'Sion'
-person2.lastName = 'Fernando'
-person2.age = '17'
-person2.city = 'Baggio'
-person2.address = 'Via Valle Atrona 5'
-
-console.log(person2.presentation())
-
-
-const Comparison = function (_firstName, _lastName,_age,_city, _address) {
-    this.firstName = _firstName
-    this.lastName = _lastName
-    this.age = _age
-    this.city = _city
-    this.address = _address
-
-    this.bigger = () => {
-        if(person1.age >= person2.age) {
-            return `${person1.firstName} ${person1.lastName} è più grande di ${person2.firstName} ${person2.lastName}`
-        } else {
-            return `${person2.firstName} ${person2.lastName} è più grande di ${person1.firstName} ${person1.lastName}`
-        }
-
-    }
-
-    this.girl = ()  => {
-        return `${this.firstName} ${this.lastName} ha ${this.age} anni e vive a ${this.city} in ${this.address}`
-    }
-}
-
-const result = new Comparison
-
-
-console.log(result.bigger()) // ricorda di richiamare una costante per ottenere il risulato//
-
-const girl1 = new Comparison('Valentina','Cornelio','15','Lacchiarella','Via Puccini 4')
-
-console.log(girl1.girl())
-
+console.log(girl1.girl());
 
 //facciamo la stessa cosa usando un costruttore //
 
-console.log('Usando un costruttore')
+console.log("Usando un costruttore");
 
-class Student{
-    constructor(_firstName,_lastName,_age,_city) {
-        this.firstName = _firstName
-        this.lastName = _lastName
-        this.age = _age
-        this.city = _city
+class Student {
+  constructor(_firstName, _lastName, _age, _city) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.city = _city;
+  }
+
+  whoIsBigger() {
+    if (person1.age > person2.age) {
+      return `${person1.firstName} ${person1.lastName} è più grande di ${person2.firstName} ${person2.lastName}`;
+    } else {
+      return `${person1.firstName} ${person1.lastName} è più piccolo/a di ${person2.firstName} ${person2.lastName}`;
     }
-
-
-    whoIsBigger() {
-        if( person1.age > person2.age) {
-            return `${person1.firstName} ${person1.lastName} è più grande di ${person2.firstName} ${person2.lastName}`
-        } else {
-            return `${person1.firstName} ${person1.lastName} è più piccolo/a di ${person2.firstName} ${person2.lastName}`
-        }
-    }
+  }
 }
 
-const result2 = new Student
+const result2 = new Student();
 
-console.log(result2.whoIsBigger())
-
+console.log(result2.whoIsBigger());
 
 //esercizio 2//
 
+const petName = document.getElementById("petName");
+const ownerName = document.getElementById("ownerName");
+const species = document.getElementById("species");
+const breed = document.getElementById("breed");
 
-const petName= document.getElementById('petName')
-const ownerName= document.getElementById('ownerName')
-const species = document.getElementById('species')
-const breed = document.getElementById('breed')
-
-const btnAdd = document.getElementById('add')
-const list = document.getElementById('list')
+const btnAdd = document.getElementById("add");
+const list = document.getElementById("list");
 const pets = [];
 
-
 class Pet {
-    constructor(_petName, _ownerName, _species, _breed) {
-        this.petName = _petName
-        this.ownerName = _ownerName
-        this.species = _species
-        this.breed = _breed
-    }
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+  }
 }
 
-
-btnAdd.addEventListener('click',function(e){
-    e.preventDefault();
-    const newPet = new Pet(petName.value, ownerName.value, species.value, breed.value)
-    pets.push(newPet)
-    printList()
-
-
-
-})
-
+btnAdd.addEventListener("click", function (e) {
+  e.preventDefault();
+  const newPet = new Pet(
+    petName.value,
+    ownerName.value,
+    species.value,
+    breed.value
+  );
+  pets.push(newPet);
+  printList();
+});
 
 function printList() {
-    list.innerHTML = ''
-    pets.forEach(pet => {
-        let newLi = document.createElement('li')
-        newLi.innerText = `PetsName : ${pet.petName}, Owner: ${pet.ownerName} Specie : ${pet.species} Breed:${pet.breed}`
-        list.appendChild(newLi)
-    })
+  list.innerHTML = "";
+  if (petName.value != " " || petName.value != "" || petName.value != null) {
+    pets.forEach((pet) => {
+      let newLi = document.createElement("li");
+      newLi.innerText = `PetsName : ${pet.petName}, Owner: ${pet.ownerName} Specie : ${pet.species} Breed:${pet.breed}`;
+      list.appendChild(newLi);
+    });
+  } else {
+    let testo = document.createElement("p");
+    testo.innerText = "Manca il Nome dell'animale";
+    list.appendChild(testo);
+  }
 
-    const petsForm = document.getElementById('petsForm')
-petsForm.reset()
+  const petsForm = document.getElementById("petsForm");
+  petsForm.reset();
 }
-
-
-
-
-
-
